789621a135676cf13302726d09f7e1338c872977
Anupam Gupta <116897217+c2gup@users.noreply.github.com>
2024-02-09 23:50:43 +0530

    Update index.js

--- JSClass2/index.js
+++ JSClass2/index.js
@@ -1,6 +1,6 @@
-console.log('Chaliye shuru karte hai ');
+console.log('Chaliye shuru karte hai '); // Start message
 
-//object create
+// Object create
 let rectangle = {
     length: 1,
     breadth: 2,
@@ -10,62 +10,58 @@ let rectangle = {
     }
 };
 
-//factory function 
-
+// Factory function 
 // function createRectangle(len, bre) {
 
-    const rectangle = {
-        length: len,
-        breadth:bre ,
+const rectangle = {
+    length: len,
+    breadth: bre,
 
-        draw() {
-            console.log('drawing rectangle');
-        }
-     };
-    return rectangle;
-}
+    draw() {
+        console.log('drawing rectangle');
+    }
+};
+return rectangle;
+// }
 
-let rectangleObj1 = createRectangle(5, 4);
-let rectangle2 = createRectangle(2,3);
-let rectangle3 = createRectangle(7,9);
+let rectangleObj1 = createRectangle(5, 4); // Creating rectangle objects
+let rectangle2 = createRectangle(2, 3);
+let rectangle3 = createRectangle(7, 9);
 
-//Camelcase -> numberOfStudents
-//constructor function -> Pascal Notation -> first letter of every word is Capital -> NumberOfStudents
-//constructor function -> prop/methods -> intialise/Define
+// Camelcase -> numberOfStudents
+// Constructor function -> Pascal Notation -> first letter of every word is Capital -> NumberOfStudents
+// Constructor function -> prop/methods -> intialise/Define
 function Rectangle(len, bre) {
     this.length = len;
     this.breadth = bre;
-    this.draw= function() {
+    this.draw = function() {
         console.log('drawing');
     }
 }
 
-// Rectangle.
+// Object creation using constructor function
+let rectangleObject = new Rectangle(4, 6);
 
-// //object creation using constrcutor function
-let rectangleObject = new Rectangle(4,6);
-
-rectangleObject.color = 'yellow';
+rectangleObject.color = 'yellow'; // Adding property
 console.log(rectangleObject);
 
-delete rectangleObject.color;
+delete rectangleObject.color; // Deleting property
 console.log(rectangleObject);
 
 let Rectangle1 = new Function(
     'length', 'breadth',
-`this.length = length;
+    `this.length = length;
 this.breadth = breadth;
 this.draw= function() {
     console.log('drawing');
 }`);
 
-// //object creation using Rectangle1
-let rect = new Rectangle1(2,3);
+// Object creation using Rectangle1
+let rect = new Rectangle1(2, 3);
 
 rect.length;
 
 console.log(rect);
-
 
 rectangle.length;
 rectangle.breadth;
@@ -100,7 +96,7 @@ console.log(a);
 console.log(b);
 
 
-let a = { value: 10};
+let a = { value: 10 };
 let b = a;
 
 a.value++;
@@ -118,7 +114,7 @@ inc(a);
 
 console.log(a);
 
-let a = {value:10 };
+let a = { value: 10 };
 
 function inc(a) {
     a.value++;
@@ -129,40 +125,39 @@ inc(a);
 console.log(a.value);
 
 let rectangle = {
-    length:2,
-    breadth:4
+    length: 2,
+    breadth: 4
 };
 
-//for-in loop 
-for(let key in rectangle ) {
-    //keys are reflected through key variable 
-    //values are reflected through rectangle[key]
-    console.log(key,rectangle[key]);
+// For-in loop 
+for (let key in rectangle) {
+    // Keys are reflected through key variable 
+    // Values are reflected through rectangle[key]
+    console.log(key, rectangle[key]);
 }
 
-//for-of loop
-for(let key of Object.entries(rectangle)) {
+// For-of loop
+for (let key of Object.entries(rectangle)) {
     console.log(key);
 }
 
-if('length' in rectangle) {
+if ('length' in rectangle) {
     console.log('Present');
-}
-else {
+} else {
     console.log('Absent');
 }
 
 
-//object - colone #1
+// Object - clone #1
 let src = {
-    a:10,
-    b:20,
-    c:30
+    a: 10,
+    b: 20,
+    c: 30
 };
 
 let dest = {};
 
-for(let key in src) {
+for (let key in src) {
     dest[key] = src[key];
 }
 
@@ -171,14 +166,14 @@ console.log(dest);
 src.a++;
 console.log(dest);
 
-Object Cloning #2
+// Object Cloning #2
 let src = {
-    a:10,
-    b:20,
-    c:30
+    a: 10,
+    b: 20,
+    c: 30
 };
 
-let src2 = { value: 25};
+let src2 = { value: 25 };
 
 let dest = Object.assign({}, src, src2);
 
@@ -188,17 +183,18 @@ src.a++;
 
 console.log(dest);
 
-Object Cloning #3
+// Object Cloning #3
 let src = {
     a: 10,
-    b:20,
-    c:30
+    b: 20,
+    c: 30
 };
 
-let dest = {...src};
+let dest = { ...src };
 
 // console.log(dest);
 
 // src.a++;
 
-// console.log(dest);
+// console.log(dest); 
+
